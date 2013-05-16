03a9a78dca4a79312cf6e2864029f81e2de0f7a7
Evan Sailer <esailer@asu.edu>
2013-05-15 22:54:07 -0500

    started the client.  can send and receive text messages

--- server/server.js
+++ server/server.js
@@ -4,40 +4,40 @@ var users = [];
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({port: 8080});
 wss.on('connection', function(ws) {
-	
+    
     ws.on('message', function(message) {
-    	if (!ws.name){
-    		console.log(message + ' joined the chat');
-    		ws.name = message;
-    		users.push(ws);
-    		ws.send('current chatters:');
-    		for (var i = 0; i < users.length; i++){
-	        	if (users[i] != ws){
-	        		ws.send(users[i].name);
-	        		users[i].send(ws.name + " has joined the chat");
-	        	}
-	        }
-    	} else {
-	        console.log('%s: %s', ws.name, message);
-	        for (var i = 0; i < users.length; i++) {
-	        	if (users[i] != ws){
-	        		users[i].send(ws.name + ": " + message);
-	        	}
-	        }
-	    }
+        if (!ws.name){
+            console.log(message + ' joined the chat');
+            ws.name = message;
+            users.push(ws);
+            ws.send('current chatters:');
+            for (var i = 0; i < users.length; i++){
+                if (users[i] != ws){
+                    ws.send(users[i].name);
+                    users[i].send(ws.name + " has joined the chat");
+                }
+            }
+        } else {
+            console.log('%s: %s', ws.name, message);
+            for (var i = 0; i < users.length; i++) {
+                if (users[i] != ws){
+                    users[i].send(ws.name + ": " + message);
+                }
+            }
+        }
     });
 
     ws.on('close', function() {
-    	if (ws.name) {
-	    	console.log(ws.name + ' left the chat');
-	    	for (var i = 0; i < users.length; i++){
-	        	if (users[i] != ws){
-	        		users[i].send(ws.name + " left the chat");
-	        	} else {
-	        		users.splice(i, 1);
-	        	}
-	        }
-	    }
+        if (ws.name) {
+            console.log(ws.name + ' left the chat');
+            for (var i = 0; i < users.length; i++){
+                if (users[i] != ws){
+                    users[i].send(ws.name + " left the chat");
+                } else {
+                    users.splice(i, 1);
+                }
+            }
+        }
     });
     ws.send('enter your name');
 });
